585d3554f07e7ee01a350a8d27f0a5b02957fbb8
Aaron Tam <aaron.tam@lge.com>
2014-06-25 00:04:38 -0700

    Fixed bug with retrieving current directory. Fixed bug with parameter order. Added some output formatting.

--- switch.sh
+++ switch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+bold=`tput bold`
+normal=`tput sgr0`
 if [ -z "$1" ]; then
 	echo "Usage: switch.sh <branch-name> <root-dir> [\"<repo>...\"]"
 	echo "<branch-name> Name of branch or tag to switch to for the specified repositories."
@@ -6,18 +8,19 @@ if [ -z "$1" ]; then
 	echo "\"<repo>\"      Optional: space delimited list of repositories to change the branch/tag for. The defaults: enyo, lib/enyo-ilib, lib/layout, lib/moonstone, lib/spotlight"
 else
 	if [ -z "$2" ]; then
-		root_dir=pwd
+		root_dir=${PWD}"/"
 	else
 		root_dir="$2"
 	fi
 	if [ -z "$3" ]; then
 		repo_names=(enyo lib/enyo-ilib lib/layout lib/moonstone lib/spotlight)
 	else
-		IFS=', ' read -a repo_names <<< $2
+		IFS=', ' read -a repo_names <<< $3
 	fi
 	for i in ${repo_names[@]}; do
-		printf "\n${i}\n"
+		printf "${bold}${i}${normal}\n"
 		path=$root_dir${i}
 		git -C $path checkout $1 && git -C $path pull
+		printf "\n"
 	done
 fi
